0574e574fe62bfc14e1793542e2cf440cbe7fd3a
Lloyd Menguito <lloydmenguito@gmail.com>
2021-11-23 09:50:14 +0800

    added updated table sql

--- saliksik_uphsl (upload).sql
+++ saliksik_uphsl (upload).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2021 at 09:39 AM
+-- Generation Time: Nov 23, 2021 at 02:49 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -42,8 +42,9 @@ CREATE TABLE `file_information` (
 --
 
 INSERT INTO `file_information` (`user_id`, `file_id`, `file_name`, `file_dir`, `file_uploader`, `status`, `research_id`) VALUES
-(88, 2, 'SM CREDIT CARD APPLICATION FORM 2021.pdf', '../uploads/6198d4664d3742.83385725.pdf', 'Marc Menguito', 'pending', 0),
-(89, 3, 'JFC COOP MPL 2021.pdf', '../uploads/619a01ee9c5a48.26106146.pdf', 'Marc Menguito', 'pending', 0);
+(89, 6, 'Sabao_Hernandez_2017.pdf', '../uploads/619b30fa0fd760.23186888.pdf', 'Marc Menguito', 'pending', 5),
+(89, 7, 'SM CREDIT CARD APPLICATION FORM 2021.pdf', '../uploads/619c45408879d8.54258257.pdf', 'Marc Menguito', 'pending', 6),
+(89, 8, 'Activity-3M-Conceptual-Model-of-the-Study (1).pdf', '../uploads/619c478274cbb2.78887302.pdf', 'Marc Menguito', 'pending', 7);
 
 -- --------------------------------------------------------
 
@@ -57,9 +58,11 @@ CREATE TABLE `research_information` (
   `researchers_category` varchar(255) NOT NULL,
   `research_unit` varchar(255) NOT NULL,
   `research_title` varchar(255) NOT NULL,
+  `research_fields` varchar(255) NOT NULL,
   `publication_month` int(11) NOT NULL,
-  `publication_date` int(11) NOT NULL,
+  `publication_day` int(11) NOT NULL,
   `publication_year` int(11) NOT NULL,
+  `coAuthors_count` int(11) NOT NULL,
   `author_first_name` varchar(255) NOT NULL,
   `author_middle_initial` varchar(255) NOT NULL,
   `author_surname` varchar(255) NOT NULL,
@@ -91,8 +94,10 @@ CREATE TABLE `research_information` (
 -- Dumping data for table `research_information`
 --
 
-INSERT INTO `research_information` (`research_id`, `resource_type`, `researchers_category`, `research_unit`, `research_title`, `publication_month`, `publication_date`, `publication_year`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_name_ext`, `author_email`, `coauthor1_first_name`, `coauthor1_middle_initial`, `coauthor1_surname`, `coauthor1_name_ext`, `coauthor1_email`, `coauthor2_first_name`, `coauthor2_middle_initial`, `coauthor2_surname`, `coauthor2_name_ext`, `coauthor2_email`, `coauthor3_first_name`, `coauthor3_middle_initial`, `coauthor3_surname`, `coauthor3_name_ext`, `coauthor3_email`, `coauthor4_first_name`, `coauthor4_middile_initial`, `coauthor4_surname`, `coauthor4_name_ext`, `coauthor4_email`) VALUES
-(1, 'dissertation', 'undergraduate', 'Basic Education', 'Research Title', 4, 17, 2006, 'Marc', 'C.', 'Menguito', 'ext', 'lloydmenguito@gmail.com', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO `research_information` (`research_id`, `resource_type`, `researchers_category`, `research_unit`, `research_title`, `research_fields`, `publication_month`, `publication_day`, `publication_year`, `coAuthors_count`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_name_ext`, `author_email`, `coauthor1_first_name`, `coauthor1_middle_initial`, `coauthor1_surname`, `coauthor1_name_ext`, `coauthor1_email`, `coauthor2_first_name`, `coauthor2_middle_initial`, `coauthor2_surname`, `coauthor2_name_ext`, `coauthor2_email`, `coauthor3_first_name`, `coauthor3_middle_initial`, `coauthor3_surname`, `coauthor3_name_ext`, `coauthor3_email`, `coauthor4_first_name`, `coauthor4_middile_initial`, `coauthor4_surname`, `coauthor4_name_ext`, `coauthor4_email`) VALUES
+(5, 'dissertation', 'undergraduate', 'Basic Education', 'Research Title', 'Educational Management, IT and Engineering, Arts and Humanities, Education and Social Sciences, Law and Justice System', 1, 1, 2021, 0, 'Marc', 'C.', 'Menguito', 'ext', 'lloydmenguito@gmail.com', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(6, 'dissertation', 'undergraduate', 'Basic Education', 'Research Title', 'Accountancy and Marketing, Educational Management, IT and Engineering, Tourism and Hospitality', 1, 1, 2021, 0, 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 'Lorenzo', 'Y', 'Menguito', 'ext', 'lorenzo.menguito@yahoo.com', 'Lorenzo2', 'Y', 'Menguito', 'text', 'lorenzo.menguito@yahoo.com', '', '', '', '', '', '', '', '', '', ''),
+(7, 'thesis', 'non_teaching_staff', 'Arts and Sciences', '21', 'Accountancy and Marketing, Educational Management, Law and Justice System', 1, 1, 2021, 2, 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 'Lorenzo', 'Y', 'Menguito', '', 'lorenzo.menguito@yahoo.com', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -148,13 +153,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `file_information`
 --
 ALTER TABLE `file_information`
-  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `research_information`
 --
 ALTER TABLE `research_information`
-  MODIFY `research_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `research_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
