caeb2f9f6cb0ec6ac5c24dc312877905ddd5f64d
Lloyd Menguito <lloydmenguito@gmail.com>
2021-11-27 14:34:01 +0800

    added line in connection.php for php to report errors in detail, removed required from hidden inputs

--- saliksik_uphsl (upload).sql
+++ saliksik_uphsl (upload).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2021 at 08:42 AM
+-- Generation Time: Nov 27, 2021 at 07:32 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -42,7 +42,66 @@ CREATE TABLE `file_information` (
 
 INSERT INTO `file_information` (`user_id`, `file_id`, `file_name`, `file_dir`, `file_uploader`, `status`) VALUES
 (89, 12, 'Machine Problem #2M.pdf', '../uploads/theses/619ef1e7e935c6.01070736.pdf', 'Marc Menguito', 'pending'),
-(89, 41, 'Assignment #11 - System Project Development Life Cycle - Menguito.pdf', '../uploads/journals/619f3e18538cf6.22180114.pdf', 'Marc Menguito', 'pending');
+(89, 42, 'Assignment #11 - System Project Development Life Cycle - Menguito.pdf', '../uploads/journals/619f3f392e4444.46357486.pdf', 'Marc Menguito', 'pending'),
+(89, 58, 'JFC COOP MPL 2021.pdf', '../uploads/infographics/61a1b5693c2f19.22613242.pdf', 'Marc Menguito', 'pending'),
+(89, 59, 'Week2-OJT-Weekly-Report (1).pdf', '../uploads/infographics/61a1cce984aea4.15427562.pdf', 'Marc Menguito', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `infographic_information`
+--
+
+CREATE TABLE `infographic_information` (
+  `file_ref_id` int(11) NOT NULL,
+  `infographic_id` int(11) NOT NULL,
+  `infographic_research_unit` varchar(255) NOT NULL,
+  `infographic_researcher_category` varchar(255) NOT NULL,
+  `infographic_publication_month` int(11) NOT NULL,
+  `infographic_publication_day` int(11) NOT NULL,
+  `infographic_publication_year` int(11) NOT NULL,
+  `infographic_title` varchar(255) NOT NULL,
+  `infographic_description` varchar(255) NOT NULL,
+  `author_first_name` varchar(255) NOT NULL,
+  `author_middle_initial` varchar(255) NOT NULL,
+  `author_surname` varchar(255) NOT NULL,
+  `author_ext` varchar(255) NOT NULL,
+  `author_email` varchar(255) NOT NULL,
+  `editor_first_name` varchar(255) NOT NULL,
+  `editor_middle_initial` varchar(255) NOT NULL,
+  `editor_surname` varchar(255) NOT NULL,
+  `editor_ext` varchar(255) NOT NULL,
+  `editor_email` varchar(255) NOT NULL,
+  `coAuthor_count` int(11) NOT NULL,
+  `coauthor1_first_name` varchar(255) NOT NULL,
+  `coauthor1_middle_initial` varchar(255) NOT NULL,
+  `coauthor1_surname` varchar(255) NOT NULL,
+  `coauthor1_ext` varchar(255) NOT NULL,
+  `coauthor1_email` varchar(255) NOT NULL,
+  `coauthor2_first_name` varchar(255) NOT NULL,
+  `coauthor2_middle_initial` varchar(255) NOT NULL,
+  `coauthor2_surname` varchar(255) NOT NULL,
+  `coauthor2_ext` varchar(255) NOT NULL,
+  `coauthor2_email` varchar(255) NOT NULL,
+  `coauthor3_first_name` varchar(255) NOT NULL,
+  `coauthor3_middle_initial` varchar(255) NOT NULL,
+  `coauthor3_surname` varchar(255) NOT NULL,
+  `coauthor3_ext` varchar(255) NOT NULL,
+  `coauthor3_email` varchar(255) NOT NULL,
+  `coauthor4_first_name` varchar(255) NOT NULL,
+  `coauthor4_middle_initial` varchar(255) NOT NULL,
+  `coauthor4_surname` varchar(255) NOT NULL,
+  `coauthor4_ext` varchar(255) NOT NULL,
+  `coauthor4_email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `infographic_information`
+--
+
+INSERT INTO `infographic_information` (`file_ref_id`, `infographic_id`, `infographic_research_unit`, `infographic_researcher_category`, `infographic_publication_month`, `infographic_publication_day`, `infographic_publication_year`, `infographic_title`, `infographic_description`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_ext`, `author_email`, `editor_first_name`, `editor_middle_initial`, `editor_surname`, `editor_ext`, `editor_email`, `coAuthor_count`, `coauthor1_first_name`, `coauthor1_middle_initial`, `coauthor1_surname`, `coauthor1_ext`, `coauthor1_email`, `coauthor2_first_name`, `coauthor2_middle_initial`, `coauthor2_surname`, `coauthor2_ext`, `coauthor2_email`, `coauthor3_first_name`, `coauthor3_middle_initial`, `coauthor3_surname`, `coauthor3_ext`, `coauthor3_email`, `coauthor4_first_name`, `coauthor4_middle_initial`, `coauthor4_surname`, `coauthor4_ext`, `coauthor4_email`) VALUES
+(58, 1, 'Arts and Sciences', 'faculty', 1, 4, 2020, 'Title', 'q123123', 'Marc', 'C.', 'Menguito', 'ext', 'lloydmenguito@gmail.com', 'Marc2', 'C.2', 'Menguito2', 'ext2', 'lloydmenguito@gmail.com2', 1, 'Marc', 'C.', 'Menguito', '2', 'lloydmenguito@gmail.com', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+(59, 2, 'Law', 'postgraduate', 1, 1, 2021, 'Title', '123', 'Lorenzo', 'Y', 'Menguito', '123', 'lorenzo.menguito@yahoo.com', 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -72,7 +131,7 @@ CREATE TABLE `journal_information` (
 --
 
 INSERT INTO `journal_information` (`file_ref_id`, `journal_id`, `journal_title`, `journal_subtitle`, `department`, `volume_number`, `serial_issue_number`, `ISSN`, `journal_description`, `chief_editor_first_name`, `chief_editor_middle_initial`, `chief_editor_last_name`, `chief_editor_name_ext`, `chief_editor_email`) VALUES
-(41, 1, 'TitleText', 'SubTitle', 'Arts and Sciences', 16, 37, '56ABC', 'desc1', 'Lorenzo', 'Y', 'Menguito', 'exten', 'lorenzo.menguito@yahoo.com');
+(42, 2, 'TitleText', 'SubTitle', 'Arts and Sciences', 16, 37, '56ABC', 'desc1', 'Lorenzo', 'Y', 'Menguito', 'exten', 'lorenzo.menguito@yahoo.com');
 
 -- --------------------------------------------------------
 
@@ -161,6 +220,12 @@ ALTER TABLE `file_information`
   ADD PRIMARY KEY (`file_id`);
 
 --
+-- Indexes for table `infographic_information`
+--
+ALTER TABLE `infographic_information`
+  ADD PRIMARY KEY (`infographic_id`);
+
+--
 -- Indexes for table `journal_information`
 --
 ALTER TABLE `journal_information`
@@ -186,13 +251,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `file_information`
 --
 ALTER TABLE `file_information`
-  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+
+--
+-- AUTO_INCREMENT for table `infographic_information`
+--
+ALTER TABLE `infographic_information`
+  MODIFY `infographic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `journal_information`
 --
 ALTER TABLE `journal_information`
-  MODIFY `journal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `journal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `research_information`
