893ca50d390ad4acaa04734bdca2c409f98d24ad
Serking de Orayom <kingdeorayom@gmail.com>
2021-11-29 09:11:47 +0800

    Add updated sql table

--- saliksik_uphsl (up to date).sql
+++ saliksik_uphsl (up to date).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 10:49 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Nov 29, 2021 at 02:11 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -80,7 +80,8 @@ CREATE TABLE `file_information` (
 
 INSERT INTO `file_information` (`user_id`, `file_id`, `file_name`, `file_dir`, `file_uploader`, `status`) VALUES
 (89, 68, 'Assignment #11 - System Project Development Life Cycle - Menguito.pdf', '../uploads/theses/61a344b61ea902.29102429.pdf', 'Marc Menguito', 'pending'),
-(89, 69, 'JFC COOP MPL 2021.pdf', '../uploads/infographics/61a34cbd3b7a55.65417639.pdf', 'Marc Menguito', 'pending');
+(89, 69, 'JFC COOP MPL 2021.pdf', '../uploads/infographics/61a34cbd3b7a55.65417639.pdf', 'Marc Menguito', 'pending'),
+(87, 70, 'DEFENSE-PPT.pdf', '../uploads/theses/61a37f02e78954.82895197.pdf', 'Serking De Orayom', 'pending');
 
 -- --------------------------------------------------------
 
@@ -117,7 +118,9 @@ CREATE TABLE `infographic_information` (
 --
 
 INSERT INTO `infographic_information` (`file_ref_id`, `infographic_id`, `infographic_research_unit`, `infographic_researcher_category`, `infographic_publication_month`, `infographic_publication_day`, `infographic_publication_year`, `infographic_title`, `infographic_description`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_ext`, `author_email`, `editor_first_name`, `editor_middle_initial`, `editor_surname`, `editor_ext`, `editor_email`, `coauthors_count`, `coauthor_group_id`) VALUES
-(69, 3, 'Arts and Sciences', 'faculty', 1, 1, 2021, '123', '12345', 'Marc', 'C.', 'Menguito', '123', 'lloydmenguito@gmail.com', 'Marc', 'C.', 'Menguito', '321', 'lloydmenguito@gmail.com', 2, 7);
+(69, 3, 'Arts and Sciences', 'faculty', 1, 1, 2021, '123', '12345', 'Marc', 'C.', 'Menguito', '123', 'lloydmenguito@gmail.com', 'Marc', 'C.', 'Menguito', '321', 'lloydmenguito@gmail.com', 2, 7),
+(73, 4, 'Basic Education', 'undergraduate', 1, 1, 2021, 'dsfdsf', 'jh', 'sdf', 'sdfsd', 'fsdf', 'sdfsdf', 'sdfdsfsd', 'sdfsdf', 'sdfsd', 'fsdf', 'dsfdsf', 'sdfdsfsdf', 4, 9),
+(83, 5, 'Basic Education', 'undergraduate', 1, 1, 2021, 'asdsads', 'asdsad', 'sdadsad', 'sadsa', 'dasd', 'asdasd', 'asdsad', 'dsadsad', 'asd', 'sadsad', 'sadsad', 'sadsadsad', 0, 16);
 
 -- --------------------------------------------------------
 
@@ -142,6 +145,13 @@ CREATE TABLE `journal_information` (
   `chief_editor_email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `journal_information`
+--
+
+INSERT INTO `journal_information` (`file_ref_id`, `journal_id`, `journal_title`, `journal_subtitle`, `department`, `volume_number`, `serial_issue_number`, `ISSN`, `journal_description`, `chief_editor_first_name`, `chief_editor_middle_initial`, `chief_editor_last_name`, `chief_editor_name_ext`, `chief_editor_email`) VALUES
+(75, 3, 'title', 'sub', 'Business and Accountancy', 2323, 2323, '2323', 'aaaa', 'aaa', 'aaa', 'aaa', 'aaa', 'aaa');
+
 -- --------------------------------------------------------
 
 --
@@ -155,6 +165,7 @@ CREATE TABLE `research_information` (
   `researchers_category` varchar(255) NOT NULL,
   `research_unit` varchar(255) NOT NULL,
   `research_title` varchar(255) NOT NULL,
+  `research_abstract` text NOT NULL,
   `research_fields` varchar(255) NOT NULL,
   `publication_month` int(11) NOT NULL,
   `publication_day` int(11) NOT NULL,
@@ -172,8 +183,10 @@ CREATE TABLE `research_information` (
 -- Dumping data for table `research_information`
 --
 
-INSERT INTO `research_information` (`file_ref_id`, `research_id`, `resource_type`, `researchers_category`, `research_unit`, `research_title`, `research_fields`, `publication_month`, `publication_day`, `publication_year`, `coauthors_count`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_name_ext`, `author_email`, `coauthor_group_id`) VALUES
-(68, 11, 'dissertation', 'undergraduate', 'Basic Education', 'Research Title', 'Accountancy and Marketing, Educational Management, IT and Engineering, Tourism and Hospitality', 1, 1, 2021, 1, 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 6);
+INSERT INTO `research_information` (`file_ref_id`, `research_id`, `resource_type`, `researchers_category`, `research_unit`, `research_title`, `research_abstract`, `research_fields`, `publication_month`, `publication_day`, `publication_year`, `coauthors_count`, `author_first_name`, `author_middle_initial`, `author_surname`, `author_name_ext`, `author_email`, `coauthor_group_id`) VALUES
+(68, 11, 'dissertation', 'undergraduate', 'Basic Education', 'Research Title', '', 'Accountancy and Marketing, Educational Management, IT and Engineering, Tourism and Hospitality', 1, 1, 2021, 1, 'Marc', 'C.', 'Menguito', '', 'lloydmenguito@gmail.com', 6),
+(72, 12, 'dissertation', 'undergraduate', 'Basic Education', 'sadsad', 'HG', 'Accountancy and Marketing, Educational Management, IT and Engineering, Tourism and Hospitality', 1, 1, 2021, 4, '0', 'sad', 'sadsad', 'sadsadsa', 'dsadsadsd', 8),
+(74, 13, 'dissertation', 'undergraduate', 'Basic Education', 'Title', 'abstract sample lorem ipsum', 'Educational Management, IT and Engineering', 1, 1, 2021, 0, '0', 'A', 'S', 'Jr', 'king@gmail.com', 10);
 
 -- --------------------------------------------------------
 
@@ -247,31 +260,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `coauthors_information`
 --
 ALTER TABLE `coauthors_information`
-  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `file_information`
 --
 ALTER TABLE `file_information`
-  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT for table `infographic_information`
 --
 ALTER TABLE `infographic_information`
-  MODIFY `infographic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `infographic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `journal_information`
 --
 ALTER TABLE `journal_information`
-  MODIFY `journal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `journal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `research_information`
 --
 ALTER TABLE `research_information`
-  MODIFY `research_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `research_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
