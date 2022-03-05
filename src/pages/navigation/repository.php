4b9af67d4ebc4500b55b1d42a4a71d8f6105169f
Serking de Orayom <kingdeorayom@gmail.com>
2022-03-05 20:52:51 +0800

    Added home page and repository page advanced search modal window

--- src/pages/navigation/repository.php
+++ src/pages/navigation/repository.php
@@ -129,19 +129,99 @@ $statement->close();
                     <h2 id="masthead-title-text">Search the repository</h2>
 
                     <div class="input-group my-3">
-                        <input type="search" class="form-control form-search rounded-0" id="repository-search-bar" aria-label="Search the repository" aria-describedby="button-addon2" <?php if(isset($_POST['title_query'])){ echo "value = '{$_POST['title_query']}'";} ?>>
+                        <input type="search" class="form-control form-search rounded-0" id="repository-search-bar" aria-label="Search the repository" <?php if(isset($_POST['title_query'])){ echo "value = '{$_POST['title_query']}'";} ?>>
                         <button class="btn text-light rounded-0 search-button btn-lg" id="repository-search-button">Search</button>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6 d-flex justify-content-center mb-3">
-                    <a href="#"><button class="btn btn-link btn-lg search-option-buttons" type="button" id="button-addon2"><i class="fas fa-search fa-2x p-3" style="color: gray;"></i><br>Advanced Search</button></a>
+                <div class="col d-flex justify-content-center mb-3">
+                    <a href="#"><button class="btn btn-link btn-lg search-option-buttons" data-bs-toggle="modal" data-bs-target="#search-modal" type="button" id=""><i class="fas fa-search fa-2x p-3" style="color: gray;"></i><br>Advanced Search</button></a>
                 </div>
-                <div class="col-md-6 d-flex justify-content-center">
-                    <a href="#"><button class="btn btn-link btn-lg search-option-buttons" type="button" id="button-addon2"><i class="fas fa-book fa-2x p-3" style="color: gray;"></i><br>Browse Researches</button></a>
+                <div class="col d-flex justify-content-center">
+                    <a href="#"><button class="btn btn-link btn-lg search-option-buttons" type="button" id=""><i class="fas fa-book fa-2x p-3" style="color: gray;"></i><br>Browse Researches</button></a>
                 </div>
             </div>
+			
+			 <!-- Modal window -->
+            <div class="modal fade" id="search-modal" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title"><i class="fas fa-search mx-2" style="color: white;"></i> Advanced Search</h5>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row my-3 px-3">
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">Find articles</h6>
+                                </div>
+                                <div class="col-6">
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">with <span class="fw-bold">all</span> of the words</h6>
+                                </div>
+                                <div class="col-6">
+                                    <input class="form-control form-control-sm rounded-0 my-1" type="text">
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">with the <span class="fw-bold">exact phrase</span></h6>
+                                </div>
+                                <div class="col-6">
+                                    <input class="form-control form-control-sm rounded-0 my-1" type="text">
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">with <span class="fw-bold">at least one</span> of the words</h6>
+                                </div>
+                                <div class="col-6">
+                                    <input class="form-control form-control-sm rounded-0 my-1" type="text">
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal"><span class="fw-bold">without</span> the words</h6>
+                                </div>
+                                <div class="col-6">
+                                    <input class="form-control form-control-sm rounded-0 my-1" type="text">
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">where my words occur</h6>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-check mt-2">
+                                        <input class="form-check-input" type="radio" name="radioButtonModal" id="radio-button-anywhere" checked>
+                                        <label class="form-check-label" for="radio-button-anywhere">anywhere in the article</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioButtonModal" id="radio-button-title">
+                                        <label class="form-check-label" for="radio-button-title">in the title of the article</label>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">Return articles <span class="fw-bold">authored</span> by</h6>
+                                </div>
+                                <div class="col-6">
+                                    <input class="form-control form-control-sm rounded-0 my-1" type="text">
+                                    <label class="fst-italic text-secondary" style="font-size: 12px;">e.g., "Dela Cruz" or Garcia</label>
+                                </div>
+                                <div class="col-6">
+                                    <h6 class="my-2 fw-normal">Return articles <span class="fw-bold">dated</span> between</h6>
+                                </div>
+                                <div class="col-6">
+                                    <div class="input-group mt-2">
+                                        <input type="text" class="form-control form-control-sm rounded-0 me-1">
+                                        <label class="mt-1">—</label>
+                                        <input type="text" class="form-control form-control-sm rounded-0 ms-1">
+                                    </div>
+                                    <label class="fst-italic text-secondary" style="font-size: 12px;">e.g., 2021</label>
+                                </div>
+                                <div class="text-center mt-4">
+                                    <button type="button" class="btn btn-primary rounded-0 modal-search-button">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
         </div>
     </section>
 
