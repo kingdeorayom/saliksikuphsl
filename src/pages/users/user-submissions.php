4d3986954d479f07f585d5dbc2c3960d99ae8cd5
Lloyd Menguito <lloydmenguito@gmail.com>
2022-03-16 18:56:19 +0800

    user submissions page

--- src/pages/users/user-submissions.php
+++ src/pages/users/user-submissions.php
@@ -31,7 +31,9 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/profile-style.css');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Submissions</title>
     <?php include_once '../../../assets/fonts/google-fonts.php' ?>
-
+    <!-- jquery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../../../scripts/custom/user-submissions.js" type="module"></script>
     <link rel="stylesheet" href="../../../styles/bootstrap/bootstrap.css" type="text/css">
     <link rel="stylesheet" href="<?php echo '../../../styles/custom/main-style.css?id=' . $maincssVersion ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo '../../../styles/custom/pages/profile-style.css?id=' . $pagecssVersion ?>" type="text/css">
@@ -53,7 +55,7 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/profile-style.css');
                     <hr class="my-4">
                     <div class="submissions">
 
-                        <div class="pendingApproval my-4">
+                        <div class="pendingApproval my-4" id="pending-container">
                             <h4 class="fw-bold" style="color: #012265;">Pending approval</h4>
                             <!-- <hr class="my-4"> -->
 
@@ -84,11 +86,9 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/profile-style.css');
 
                         </div>
 
-                        <div class="forRevision my-3">
-
+                        <div class="forRevision my-3"  id="revision-container">
                             <h4 class="fw-bold" style="color: #012265;">For revision</h4>
                             <!-- <hr class="my-4"> -->
-
                             <div class="box p-3 my-3" style="background-color: #f5f5f5;">
                                 <div class="row">
                                     <div class="col">
@@ -129,7 +129,7 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/profile-style.css');
                     <div class="row">
                         <h1 class="my-1">Published Works</h1>
                         <div class="published">
-                            <div class="publishedWorks">
+                            <div class="publishedWorks"  id="published-container">
                                 <hr class="mt-3 mb-4">
 
                                 <div class="box p-3 my-3" style="background-color: #f5f5f5;">
