5ff2dfad641d97aa71dc9f11e8124b47b8466bcf
Serking de Orayom <kingdeorayom@gmail.com>
2022-03-28 09:04:42 +0800

    added delete button on researcher profile

--- src/pages/navigation/view-researcher.php
+++ src/pages/navigation/view-researcher.php
@@ -10,31 +10,32 @@ if (!isset($_SESSION['isLoggedIn'])) {
     die();
 }
 
-if(!isset($_GET['id'])){
+if (!isset($_GET['id'])) {
     die();
 }
 
 $statement = $connection->prepare("SELECT * FROM researcher_profile WHERE researcher_id = ?");
-$statement->bind_param("i",$_GET['id']);
+$statement->bind_param("i", $_GET['id']);
 $statement->execute();
 $result = $statement->get_result();
 $researcher = $result->fetch_assoc();
 $statement->close();
 
 $statement = $connection->prepare("SELECT * FROM researcher_works WHERE researcher_ref_id = ?");
-$statement->bind_param("i",$_GET['id']);
+$statement->bind_param("i", $_GET['id']);
 $statement->execute();
 $result = $statement->get_result();
 $published_works = $result->fetch_all(MYSQLI_ASSOC);
 $statement->close();
 
-function filter(&$value){
+function filter(&$value)
+{
     $value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
 $maincssVersion = filemtime('../../../styles/custom/main-style.css');
 $pagecssVersion = filemtime('../../../styles/custom/pages/researchers-style.css');
-$imageVersion = filemtime("../".$researcher['researcher_image']);
+$imageVersion = filemtime("../" . $researcher['researcher_image']);
 ?>
 
 <!DOCTYPE html>
@@ -77,48 +78,59 @@ $imageVersion = filemtime("../".$researcher['researcher_image']);
             </div>
             <div class="row">
                 <div class="col-lg-2 p-2">
-                    <img src="../<?php echo htmlspecialchars($researcher['researcher_image'])."?id=".$imageVersion ?>" class="img-fluid">
+                    <img src="../<?php echo htmlspecialchars($researcher['researcher_image']) . "?id=" . $imageVersion ?>" class="img-fluid" width="140" height="140">
                 </div>
                 <div class="col-lg-10">
                     <h2 class="fw-bold mt-3"><?php echo htmlspecialchars($researcher['name']) ?>
-                    <?php if($_SESSION['userType'] == 'admin'){
-                        echo "<a href='./edit-researcher-profile.php?id={$researcher['researcher_id']}' class='edit-profile-button'><i class='fas fa-edit h5'></i></a></h2>";
-                    } ?>
-                    <h5 class="mb-2"><?php echo htmlspecialchars($researcher['type']) ?></h5>
+                        <?php if ($_SESSION['userType'] == 'admin') {
+                            echo "<a href='./edit-researcher-profile.php?id={$researcher['researcher_id']}' class='edit-profile-button'><i class='fas fa-edit h5'></i></a></h2>";
+                        } ?>
+                        <h5 class="mb-2"><?php echo htmlspecialchars($researcher['type']) ?></h5>
 
-                    <div class="row my-5">
-                        <div class="col-lg-3 border-top border-2">
-                            <h6 class="fw-bold my-3">College/Department</h6>
-                        </div>
-                        <div class="col-lg-9 border-top border-2">
-                            <p class="my-3"><?php echo htmlspecialchars($researcher['department']) ?></p>
-                        </div>
-                        <div class="col-lg-3 border-top border-2">
-                            <h6 class="fw-bold my-3">Highest Educational Attainment</h6>
-                        </div>
-                        <div class="col-lg-9 border-top border-2">
-                            <p class="my-3"><?php echo htmlspecialchars($researcher['highest_edu_attainment']) ?></p>
-                        </div>
-                        <div class="col-lg-3 border-top border-2">
-                            <h6 class="fw-bold my-3">Research Interest</h6>
-                        </div>
-                        <div class="col-lg-9 border-top border-2">
-                            <p class="my-3"><?php echo htmlspecialchars($researcher['research_interest']) ?></p>
-                        </div>
-                    </div>
+                        <div class="row my-5">
+                            <div class="col-lg-3 border-top border-2">
+                                <h6 class="fw-bold my-3">College/Department</h6>
+                            </div>
+                            <div class="col-lg-9 border-top border-2">
+                                <p class="my-3"><?php echo htmlspecialchars($researcher['department']) ?></p>
+                            </div>
+                            <div class="col-lg-3 border-top border-2">
+                                <h6 class="fw-bold my-3">Highest Educational Attainment</h6>
+                            </div>
+                            <div class="col-lg-9 border-top border-2">
+                                <p class="my-3"><?php echo htmlspecialchars($researcher['highest_edu_attainment']) ?></p>
+                            </div>
+                            <div class="col-lg-3 border-top border-2">
+                                <h6 class="fw-bold my-3">Research Interest</h6>
+                            </div>
+                            <div class="col-lg-9 border-top border-2">
+                                <p class="my-3"><?php echo htmlspecialchars($researcher['research_interest']) ?></p>
+                            </div>
 
-                    <div class="row my-5">
-                        <div class="col">
+                        </div>
 
-                            <h3 class="fw-bold">Published Works</h3>
-                            <hr>
-                            <?php foreach($published_works as $key => $result){
-                                array_walk_recursive($result,"filter");
-                                echo "<a href='{$result['research_link']}' class='my-4 published-works h5'>{$result['research_title']}</a>
+                        <div class="row my-5">
+                            <div class="col">
+
+                                <h3 class="fw-bold">Published Works</h3>
+                                <hr>
+                                <?php foreach ($published_works as $key => $result) {
+                                    array_walk_recursive($result, "filter");
+                                    echo "<a href='{$result['research_link']}' class='my-4 published-works h5'>{$result['research_title']}</a>
                                 <hr>";
-                            }?>
+                                } ?>
+                            </div>
+
                         </div>
-                    </div>
+
+
+                        <div class="row my-5">
+                            <div class="text-start">
+                                <p class="fst-italic text-danger"><span class="fw-bold">IMPORTANT:</span> This will delete all data and records for this researcher. Proceed with caution.</p>
+                                <button class="btn btn-danger rounded-0"><i class='fas fa-trash-alt'></i> Delete profile</button>
+
+                            </div>
+                        </div>
                 </div>
             </div>
 
