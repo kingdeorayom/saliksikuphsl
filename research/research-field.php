3a46b6d148316400d0809fd3e3802862c30122bb
Lloyd Menguito <lloydmenguito@gmail.com>
2022-04-02 13:31:41 +0800

    research field test

--- research/research-field.php
+++ research/research-field.php
@@ -2,10 +2,28 @@
 
 session_start();
 
+include '../includes/connection.php';
+
 if (!isset($_SESSION['isLoggedIn'])) {
     header("location: ../../layouts/general/error.php");
     die();
 }
+
+if(!isset($_GET['query'])){
+    die();
+}
+
+$query = "SELECT fi.*,`research_id`,`resource_type`,`researchers_category`,`research_unit`,`research_title`,`research_abstract`,`research_fields`,`keywords`,`publication_date`,ri.coauthors_count AS `research_coauthors_count`,ri.author_first_name AS researcher_first_name, ri.author_middle_initial AS researcher_middle_initial, ri.author_surname AS researcher_surname, ri.author_name_ext AS researcher_name_ext, ri.author_email AS researcher_email, ci.* FROM file_information AS fi LEFT JOIN research_information as ri ON ri.file_ref_id=fi.file_id LEFT JOIN coauthors_information AS ci on ci.group_id = fi.coauthor_group_id WHERE fi.status = 'published' && research_fields LIKE %?%";
+$statement = $connection->prepare($query);
+$statement->bind_param("s",$_GET['query']);
+$statement->execute();
+$result = $statement->get_result();
+$published = $result->fetch_all(MYSQLI_ASSOC);
+
+print_r($published);
+$statement->close();
+
+$statement = $connection->prepare("SELECT * FROM ");
 $maincssVersion = filemtime('../../../styles/custom/main-style.css');
 $pagecssVersion = filemtime('../../../styles/custom/pages/home-style.css');
 ?>
@@ -27,7 +45,7 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/home-style.css');
 
 <body>
 
-    <?php include_once '../../layouts/general/header.php' ?>
+    <?php include_once '../includes/header.php' ?>
 
     <section class=" masthead p-5 bg-light">
         <div class="container">
@@ -85,7 +103,7 @@ $pagecssVersion = filemtime('../../../styles/custom/pages/home-style.css');
         </div>
     </section>
 
-    <?php include_once '../../layouts/general/footer.php' ?>
+    <?php include_once '../includes/footer.php' ?>
 
     <script src="https://kit.fontawesome.com/dab8986b00.js" crossorigin="anonymous"></script>
     <script src="../../../scripts/bootstrap/bootstrap.js"></script>
