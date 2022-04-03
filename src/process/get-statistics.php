811733a619936724f943803c8c2067398ab3d734
Lloyd Menguito <lloydmenguito@gmail.com>
2022-04-04 06:41:51 +0800

    statistics query

--- src/process/get-statistics.php
+++ src/process/get-statistics.php
@@ -7,7 +7,23 @@ if (mysqli_connect_errno()) {
     exit("Failed to connect to the database: " . mysqli_connect_error());
 };
 
-$statement = $connection->prepare("SELECT YEAR(fi.published_on) AS year, COUNT(fi.file_id) AS count FROM file_information AS fi GROUP BY YEAR(fi.submitted_on)");
+$statement = $connection->prepare("SELECT Year, SUM(t.count) FROM (SELECT YEAR(ri.publication_date) AS Year, COUNT(fi.file_id) AS count 
+FROM file_information AS fi LEFT JOIN research_information AS ri ON ri.file_ref_id = fi.file_id GROUP BY Year
+
+UNION ALL 
+
+SELECT YEAR(ii.infographic_publication_date) AS Year, COUNT(fi.file_id) AS count
+FROM file_information AS fi LEFT JOIN infographic_information AS ii ON ii.file_ref_id = fi.file_id GROUP BY Year
+           
+UNION ALL 
+
+SELECT YEAR(fi.published_on) AS Year, COUNT(fi.file_id) AS count
+FROM file_information AS fi WHERE fi.file_type = 'journal' GROUP BY Year
+                            
+UNION ALL 
+
+SELECT YEAR(rp.report_year) AS Year, COUNT(fi.file_id) AS count
+FROM file_information AS fi LEFT JOIN reports_information AS rp ON rp.file_ref_id = fi.file_id GROUP BY Year) as t GROUP BY t.Year");
 $statement->execute();
 $result = $statement->get_result();
 $published_by_year = $result->fetch_all(MYSQLI_ASSOC);
