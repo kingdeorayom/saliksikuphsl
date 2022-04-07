95fc1f3f402d90ce95dada7fbe123e936da0ac07
Serking de Orayom <kingdeorayom@gmail.com>
2022-04-08 02:01:41 +0800

    footer font fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,32 @@
 <!--Footer-->
 
-<section class="footer p-5">
+<section class="footer px-5 py-3">
     <div class="container">
         <div class="row">
             <div class="col-lg-5 my-1">
                 <a href="/home.php"><img src="/assets/images/core/saliksik-logo.png" alt="" class="img-fluid saliksik-logo mb-1"></a>
-                <p class="footer-column-title lead fw-bold">University of Perpetual Help System Laguna</p>
+                <p class="footer-column-title fw-bold">University of Perpetual Help System Laguna</p>
                 <ul class="footer-list">
-                    <li><a class="footer-list-link lead">UPH Compound, National Highway,</a></li>
-                    <li><a class="footer-list-link lead">Sto. Niño, City of Biñan, Laguna</a></li>
+                    <li><a class="footer-list-link">UPH Compound, National Highway,</a></li>
+                    <li><a class="footer-list-link">Sto. Niño, City of Biñan, Laguna</a></li>
                 </ul>
             </div>
             <div class="col-lg-4 my-1">
-                <p class="footer-column-title lead">External Links</p>
+                <p class="footer-column-title">External Links</p>
                 <ul class="footer-list">
-                    <li><a class="footer-list-link lead ext-link" href="http://gti-binan.dyndns.org:8339/index.htm" target="_blank">School Automate</a></li>
-                    <li><a class="footer-list-link lead ext-link" href="https://lmsbinan.uphsl.edu.ph/moodle/" target="_blank">LMS Biñan (Moodle)</a></li>
-                    <li><a class="footer-list-link lead ext-link" href="http://gti-binan.dyndns.org:8339/lms/" target="_blank">Online Library</a></li>
-                    <li><a class="footer-list-link lead ext-link" href="https://uphsl.edu.ph" target="_blank">University Website</a></li>
+                    <li><a class="footer-list-link ext-link" href="http://gti-binan.dyndns.org:8339/index.htm" target="_blank">School Automate</a></li>
+                    <li><a class="footer-list-link ext-link" href="https://lmsbinan.uphsl.edu.ph/moodle/" target="_blank">LMS Biñan (Moodle)</a></li>
+                    <li><a class="footer-list-link ext-link" href="http://gti-binan.dyndns.org:8339/lms/" target="_blank">Online Library</a></li>
+                    <li><a class="footer-list-link ext-link" href="https://uphsl.edu.ph" target="_blank">University Website</a></li>
                 </ul>
             </div>
             <div class="col-lg-3 my-1">
-                <p class="footer-column-title lead fw-bold">CONTACT US</p>
+                <p class="footer-column-title fw-bold">CONTACT US</p>
                 <ul class="footer-list">
-                    <li><a class="footer-list-link lead fw-bold">Research and Development Center</a></li>
-                    <li><a class="footer-list-link lead">Room 247 College Building, UPH Compound</a></li>
-                    <li><a class="footer-list-link lead">research@uphsl.edu.ph</a></li>
-                    <li><a class="footer-list-link lead">049-544-5162</a></li>
+                    <li><a class="footer-list-link fw-bold">Research and Development Center</a></li>
+                    <li><a class="footer-list-link">Room 247 College Building, UPH Compound</a></li>
+                    <li><a class="footer-list-link">research@uphsl.edu.ph</a></li>
+                    <li><a class="footer-list-link">049-544-5162</a></li>
                 </ul>
             </div>
         </div>
